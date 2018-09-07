e85945294fbb47fe3e829acb8d93c814d9e2153c
Aaron Biller <aaron@postmates.com>
2018-09-07 00:45:29 -0400

    Add bigquery, split out db models

---
+++ comparator/db/redshift.py
@@ -0,0 +1,11 @@
+"""Class for using Redshift as a source database
+"""
+from comparator.db.postgres import PostgresDb
+
+
+class RedshiftDb(PostgresDb):
+    _db_type = 'redshift+psycopg2'
+
+    def __init__(self, *args, **kwargs):
+        self._conn_kwargs['port'] = 5439
+        super(RedshiftDb, self).__init__(*args, **kwargs)
