2ce0a0bed4e06c68504ec4a78cb21602e98c1af7
Aaron Biller <aaronbiller@gmail.com>
2018-09-11 21:59:09 -0400

    Update setup.py for pypi (#8)

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+import pathlib
+
 from setuptools import setup, find_packages
+
+cwd = pathlib.Path(__file__).resolve().parent
+long_description = cwd.joinpath('README.md').read_text()
 
 
 setup(
     name='comparator',
-    version='0.1.1',
+    version='0.1.2',
     author='Aaron Biller',
     author_email='aaronbiller@gmail.com',
     description='Utility for comparing results between data sources',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='Apache 2.0',
     keywords='utility compare database',
     url='https://github.com/aaronbiller/comparator',
@@ -34,6 +41,9 @@ setup(
     include_package_data=True,
     scripts=[],
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Database',
         'Topic :: Utilities',
     ],
